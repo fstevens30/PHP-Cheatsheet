9e7f2e3ff5fb53dae73553dc7bbbdb097ae64a6b
Flynn Stevens <99843472+fstevens30@users.noreply.github.com>
2022-08-23 09:40:35 +1200

    Update web502_notes.php

--- web502_notes.php
+++ web502_notes.php
@@ -19,3 +19,30 @@ function simple_load_script() {
     wp_enqueue_script( 'custom-script' );
 }
 // END Example 1.1
+
+// START Example 1.2
+/** Load a javasctipt called custom.js located in the active theme's /js directory using separate actions for registering and enqueueing **/
+// Register JavaScripts
+add_action( 'wp_enqueue_scripts', 'custom_register_scripts' );
+function custom_register_scripts() {
+    $src = get_stylesheet_directory_uri() . '/js/custom.js' ;
+    wp_register_script( 'custom-script', $src, array( 'jquery' ), '1', TRUE );
+}
+// Enqueue JavaScripts
+add_action( 'wp_enqueue_scripts', 'custom_enqueue_scripts' );
+function custom_enqueue_scripts() {
+    wp_enqueue_script( 'custom-script' );
+}
+// END Example 1.2
+
+// START Example 1.3
+/** Load a javasctipt called custom.js located in the active theme's /js directory only for pages **/
+add_action( 'wp_enqueue_scripts', 'custom_script_for_pages' );
+function custom_script_for_pages() {
+    
+    $src = get_stylesheet_directory_uri() . '/js/custom.js' ;
+    wp_register_script( 'custom-script', $src, array( 'jquery' ), '1', TRUE );
+    if ( is_page() )
+        wp_enqueue_script( 'custom-script' );
+}
+// END Example 1.3
